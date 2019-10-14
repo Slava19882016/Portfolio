433ab0335bd0d91a7ad4392c579cb46c98da6869
Slava19882016 <vp1891719@gmail.com>
2019-10-14 19:34:35 +0300

    Updates

---
+++ precache-manifest.1ce324cd483774e064265fd6347c1f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8278f10b7e42d911be23f61368673f55",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ef4a7c56c07ce6d5d3dd",
+    "url": "/Portfolio/static/css/main.52b60add.chunk.css"
+  },
+  {
+    "revision": "b6425ea7646b49ef5a0d",
+    "url": "/Portfolio/static/js/2.99804230.chunk.js"
+  },
+  {
+    "revision": "ef4a7c56c07ce6d5d3dd",
+    "url": "/Portfolio/static/js/main.b963f4fb.chunk.js"
+  },
+  {
+    "revision": "a4e9b736d210637e0c64",
+    "url": "/Portfolio/static/js/runtime-main.e013f000.js"
+  },
+  {
+    "revision": "504aa148491530bed31d8f94177a2ea3",
+    "url": "/Portfolio/static/media/IMG_1868.504aa148.JPG"
+  }
+]);
